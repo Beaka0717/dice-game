db487aea1d5933b45ced0cbf433608f1b6c560ce
Minjee <Minjee9009@gmail.com>
2022-04-07 15:26:16 +0900

    shoog shiddeg bolgolo

--- app.js
+++ app.js
@@ -6,14 +6,26 @@ var scores = [0, 0];
 //Eeljiin onoog tsugluulah huwisagch( shoo ni 1 buuwal awsan onoog ni 0lono)
 var roundScore = 0;
 //shoonii ali talaara buusniig hadgalah huwisagch(1-6 gsn utgiig ene huwisagchid sanamsarguiger uusgej ogno.)
-var dice = Math.floor(Math.random()*6)+1;
-// togloom ehlehed beldi
-document.querySelector("#score-0").textContent=0;
-document.querySelector("#score-1").textContent=0;
-document.querySelector("#current-0").textContent=0;
-document.querySelector("#current-1").textContent=0;
-document.querySelector(".dice").style.display = "none";
 
-console.log("Dice :" + dice);
+
+// togloom ehlehed beldi
+document.getElementById("score-0").textContent=0;
+document.getElementById("score-1").textContent=0;
+
+document.getElementById("current-0").textContent=0;
+document.getElementById("current-1").textContent=0;
+
+  var diceDom = document.querySelector(".dice");
+  diceDom.style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", function(){
+    var diceNumber = Math.floor(Math.random()*6)+1;
+    
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + diceNumber + ".png"
+});
+
+
+
 
 
